e53311e57a3507c7cfd6810e9b2e164e27807455
Grant Hames-Morgan <nthmorgan@gmail.com>
2018-07-10 20:34:19 -0700

    touch up hyperscript notes

--- hyperscript.js
+++ hyperscript.js
@@ -1,7 +1,7 @@
 // rewrite of https://github.com/hyperhype/hyperscript/blob/master/index.js
-// while trying to understand existing implementations:
+// while trying to understand existing implementations
 
-// this is not as terse as other rewrites because it was the first
+// drop their support for RxJS observables
 
 export default function h() {
   const args = [].slice.call(arguments)
@@ -17,43 +17,28 @@ export default function h() {
     // arguments can be a complex tag, attribute object, or children
     // note, most implementations use a stack and call `.pop()`
     const arg = args.shift()
-    if (arg === null) {
+
+    if (arg === null || (type === 'boolean' && arg === false)) {
+      // don't render
       continue
     }
     const type = typeof arg
-    if (type === 'string') {
-      if (!parent) {
-        const match = arg.split(/([\.#]?[^\s#.]+)/)
-        if (/^\.|#/.test(match[1])) {
-          parent = document.createElement('div')
+    if (type === 'string' && !parent) {
+      const match = arg.split(/([\.#]?[^\s#.]+)/)
+      if (/^\.|#/.test(match[1])) {
+        parent = document.createElement('div')
+      }
+      match.forEach(tag => {
+        if (tag && !parent) {
+          document.createElement(tag)
         }
-        match.forEach(tag => {
-          if (tag && !parent) {
-            document.createElement(tag)
-          }
-          else if (tag[0] === '#') {
-            parent.id = tag.substring(1)
-          }
-          else if (tag[0] === '.') {
-            parent.classList.add(tag.substring(1))
-          }
-        })
-      }
-      else {
-        // TODO: factor this into the below one
-        parent.appendChild(document.createTextNode(arg))
-      }
-    }
-    else if (type === 'number'
-      || type === 'boolean' // TODO: not wanted! React filters false tags
-      || arg instanceof Date
-      || arg instanceof RegExp // TODO: why did they support regex?
-    ) {
-      parent.appendChild(
-        document.createTextNode(
-          arg.toString() // this is unnecessary. toString is called by default
-        )
-      )
+        else if (tag[0] === '#') {
+          parent.id = tag.substring(1)
+        }
+        else if (tag[0] === '.') {
+          parent.classList.add(tag.substring(1))
+        }
+      })
     }
     else if (Array.isArray(arg)) {
       // pushing seems to be faster than concat
@@ -66,7 +51,6 @@ export default function h() {
       for (const [key, value] in Object.entries(arg)) {
         const keyType = typeof value
         if (keyType === 'function' && /^on\w+/.test(key)) {
-          // knowingly ignoring RxJS observable support
           parent.addEventListener(key.substring(2), value, false)
           undoMethods.push(() => {
             parent.removeEventListener(key.substring(2), value, false)
@@ -95,11 +79,12 @@ export default function h() {
       }
     }
     else if (type === 'function') {
-      // knowingly dropping observable support, again
-
       // from vhtml: "component support"
       // (attrs || attrs = {}).children = stack.reverse()
       return name(attrs, args.reverse()) // TODO: why reverse? why return?
+    }
+    else {
+      parent.appendChild(document.createTextNode(arg))
     }
   }
 }
