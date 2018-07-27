b943b0183229f5b3b9d55d0f44a1eae0f6927869
Grant Hames-Morgan <nthmorgan@gmail.com>
2018-07-26 17:30:48 -0700

    don't use uglify-es/domprops.json
    it's too extensive. covers "tag" which should be mangled

--- minify.mjs
+++ minify.mjs
@@ -1,28 +1,31 @@
 import fs from 'fs'
 import UglifyES from 'uglify-es'
-import reservedDOMProps from 'uglify-es/tools/domprops.json'
 
-const cacheFile = 'minify-cache.json'
-const nameCache = fs.existsSync(cacheFile)
-  ? JSON.parse(fs.readFileSync(cacheFile, 'utf8'))
-  : {}
-
-// domprops.json is extensive; even covering "tag", which I want to mangle
-reservedDOMProps.splice(reservedDOMProps.indexOf('tag'), 1)
+const reservedDOMProps = [
+  'createElement',
+  'createDocumentFragment',
+  'createTextNode',
+  'id',
+  'style',
+  'cssText',
+  'className',
+  'setAttribute',
+  'addEventListener',
+  'appendChild',
+]
 
 const options = {
   ecma: 8,
   mangle: {
     reserved: ['v'],
     properties: {
-      reserved: ['fragment', 'events'].concat(reservedDOMProps),
+      reserved: ['fragment', 'events', ...reservedDOMProps]
     },
   },
   toplevel: true,
   output: {
     beautify: false,
   },
-  nameCache,
 }
 
 const content = fs.readFileSync('voko.js', 'utf-8')
@@ -36,4 +39,3 @@ const reduced = code
   .slice(0, -("export{v};".length))
 
 fs.writeFileSync('voko.min.js', `window.v=(()=>{${reduced}return v})()\n`)
-fs.writeFileSync(cacheFile, JSON.stringify(options.nameCache, null, 2), 'utf8')
